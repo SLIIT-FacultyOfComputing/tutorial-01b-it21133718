615cd804c8f7984ad5db25252a743a0c246cd71a
IT21133718 <87368083+it21133718@users.noreply.github.com>
2021-07-14 06:11:11 +0000

    Tute01 Excercise 03 Calculate the sum of the numbers from 1 to 10.where is the keyboard input

--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,19 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+ int num,count=1,total=0;//Intialize & define the variable
+
+printf("Enter the number:");//Enter the input number
+scanf("%d",&num);//Read the input number
+
+while(count<=num)//Repetition
+{
+  total=total+count;//Calculation for the finf the total value of the numbers
+  count++;
+}
+printf("Sum is:%d",total);//print the sum of the 1-n number
+
+
   return 0;
 }
-
+//end of the main function
--- Tute04.c
+++ Tute04.c
@@ -5,6 +5,9 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+float minimum(int num1,int num2);
+float maximum(int num1,int num2);
+float multiply(int num1,int num2);
 
 int main() {
    int no1, no2;
@@ -17,3 +20,26 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+float minimum(int num1,int num2)
+{
+   if(num1>num1)
+
+   
+
+   return minimum;
+}
+float maximum(int num1,int num2)
+{
+
+   return maximum;
+}
+float multiply(int num1,int num2)
+{
+
+  return multiply;
+}
+
+
+return 0;
+
+}//End of the programme
