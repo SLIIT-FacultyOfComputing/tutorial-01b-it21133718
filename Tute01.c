1bfc3687ac145c4edf453120dbcb5a5f9673c075
IT21133718 <87368083+it21133718@users.noreply.github.com>
2021-07-14 04:36:55 +0000

    Tute1 Excercise1 Enter two marks & find average

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,21 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1,mark2;
+  float sum,average;
+
+  printf("Enter mark1:");
+  scanf("%d",&mark1);
+
+  printf("enter mark2:");
+  scanf("%d",&mark2);
+
+  sum=mark1+mark2;
+  average=sum/2;
+
+  printf("Average :%.2f",average);
+
   
   return 0;
 }
